9126f1b3ac1e46b404bf6b9de822f4243eeb1025
Kimchhorng <skimchhorng@gmail.com>
2017-07-24 22:11:29 +0700

    init app

--- app/__init__.py
+++ app/__init__.py
@@ -47,4 +47,7 @@ def makeWebhookResult(req):
         if action == "weather.now":
             result = getWeather(parameters)
 
+        if action == "calendar.view"
+            result == getCalendar(parameters)
+
     return result
